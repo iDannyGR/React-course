d9414d4dac3209745346d16a93b76f4738de9d88
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-09-29 15:53:45 -0600

    update introduction use bootstrap css

--- 03-detalles/src/components/Calc.jsx
+++ 03-detalles/src/components/Calc.jsx
@@ -1,19 +1,12 @@
-import React, { useState} from 'react'
+import React from 'react';
 import Inputs from './Inputs';
-import './Inputs.css'
-import Results from './Results';
+import './Inputs.css';
+
 
 const Calc = () => {
-  const [suma, setsuma] = useState(0);
-
   return (
       <div>
-        <Inputs  name="Numero 1"/>
-          <Inputs name="Numero 2" />
-          <Results operacion='Suma' calculo={suma} />
-          <Results operacion='Resta' calculo={suma} />
-          <Results operacion='Multiplicacion' calculo={suma} />
-          <Results operacion='division' calculo={suma} />
+          <Inputs  name="Numero 1"/>  
       </div>
   )
 }
