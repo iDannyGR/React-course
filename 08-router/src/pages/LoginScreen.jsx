37a43737d264939e024d6b7c3fc9b4b758004e5a
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-11-08 20:22:56 -0600

    home add login screen 8/11/22

---
+++ 08-router/src/pages/LoginScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+      <div className='container mt-5 text-center'>
+          
+    </div>
+  )
+}
+
+export default LoginScreen
--- 08-router/src/routes/loginRouter.jsx
+++ 08-router/src/routes/loginRouter.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch , Route} from 'react-router-dom';
+import LoginScreen from '../pages/LoginScreen';
 
 const loginRouter = () => {
   return (
       <Router>
           <Switch>
-              
+            <Route exact path="/login" component={ LoginScreen } />
           </Switch>
-        </Router>
+      </Router>
   )
 }
 
