8fda95e0cfef241d7ce26c9c4f6c720eb17c49b9
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-11-11 16:00:21 -0600

    alcaldia

--- 08-router/src/components/LoginForm.jsx
+++ 08-router/src/components/LoginForm.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-const LoginForm = props => {
+const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <section className="vh-100">
+  <div className="container-fluid h-custom">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div className="divider d-flex align-items-center my-4">
+            <p className="text-center fw-bold mx-3 mb-0">Login Form</p>
+          </div>
+
+          <div className="form-outline mb-4">
+            <input type="email" id="form3Example3" className="form-control form-control-lg"
+              placeholder="Enter a valid email address" />
+            <label className="form-label" for="form3Example3">Email address</label>
+          </div>
+          <div className="form-outline mb-3">
+            <input type="password" id="form3Example4" className="form-control form-control-lg"
+              placeholder="Enter password" />
+            <label className="form-label" for="form3Example4">Password</label>
+          </div>
+          <div className="text-center text-lg-start mt-4 pt-2">
+            <button type="button" className="btn btn-primary btn-lg">Login</button>
+            <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                className="link-danger">Register</a></p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
+</section>
+ 
   )
 }
-
-LoginForm.propTypes = {}
-
 export default LoginForm
--- 08-router/src/pages/LoginScreen.jsx
+++ 08-router/src/pages/LoginScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm';
 
 
 const LoginScreen = () => {
   return (
       <div className='container mt-5 text-center'>
-          
+          <LoginForm />
     </div>
   )
 }
