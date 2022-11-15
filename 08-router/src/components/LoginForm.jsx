c6f8bcd5f3597f3bf50d9abc59922bf4e5d91454
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-11-14 20:55:29 -0600

    home add login screen 14/11/22

--- 08-router/src/components/LoginForm.jsx
+++ 08-router/src/components/LoginForm.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
+import React from 'react';
+
 
 const LoginForm = () => {
   return (
-    <section className="vh-100">
-  <div className="container-fluid h-custom">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+<section className="vh-95 shadow-lg">
+  <div className="container py-5 h-100">
+    <div className="row d-flex align-items-center justify-content-center h-100">
+      <div className="col-md-8 col-lg-7 col-xl-6">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          className="img-fluid" alt=''/>
+      </div>
+      <div className="col-md-7 col-lg-5 col-xl-5 ">
         <form>
-          <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0">Login Form</p>
+          <div className="mb-4">
+            <input type="email" id="form1Example13" className="form-control form-control-lg" />
+            <label className="form-label" for="form1Example13">Email address</label>
           </div>
-
           <div className="form-outline mb-4">
-            <input type="email" id="form3Example3" className="form-control form-control-lg"
-              placeholder="Enter a valid email address" />
-            <label className="form-label" for="form3Example3">Email address</label>
+            <input type="password" id="form1Example23" className="form-control form-control-lg" />
+            <label classNameName="form-label" for="form1Example23">Password</label>
           </div>
-          <div className="form-outline mb-3">
-            <input type="password" id="form3Example4" className="form-control form-control-lg"
-              placeholder="Enter password" />
-            <label className="form-label" for="form3Example4">Password</label>
+          <div className="d-flex justify-content-around align-items-center mb-4">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+              <label className="form-check-label" for="form1Example3"> Remember me </label>
+            </div>
+            <a href="#!">Forgot password?</a>
           </div>
-          <div className="text-center text-lg-start mt-4 pt-2">
-            <button type="button" className="btn btn-primary btn-lg">Login</button>
-            <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                className="link-danger">Register</a></p>
-          </div>
-
+          <button type="submit" className="btn btn-primary btn-lg btn-block">Sign in</button>
         </form>
       </div>
     </div>
   </div>
-
 </section>
- 
   )
 }
 export default LoginForm
